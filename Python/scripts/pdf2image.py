cfb1b67e742d9d7ce2bf5c79d9d8ab39f1c3be5c
liwei <unix.lw@qq.com>
2022-04-26 18:15:04 +0800

    pdf2image

--- Python/scripts/pdf2image.py
+++ Python/scripts/pdf2image.py
@@ -1,26 +1,27 @@
+'''
+pip install fitz
+pip install PyMuPDF
+'''
+
 import fitz
 
-# pip install fitz
-# pip install PyMuPDF
-
-
-def pdf_image(pdfPath, imgPath, zoom_x, zoom_y, rotation_angle):
+def pdf_image(pdf_path, img_save_dir, zoom_x, zoom_y, rotation_angle):
     """按页生成每页的图片
 
     Args:
-        pdfPath (str): pdf路经
-        imgPath (str): 图片存放路经
+        pdf_path (str): pdf全路经
+        img_save_dir (str): 图片存放路经
         zoom_x (int): 控制分辨率
         zoom_y (int): 控制分辨率
         rotation_angle (int): _description_
     """
 
-    pdf = fitz.open(pdfPath)
+    pdf = fitz.open(pdf_path)
     for pg in range(0, pdf.pageCount):
         page = pdf[pg]
         trans = fitz.Matrix(zoom_x, zoom_y).prerotate(rotation_angle)
         pm = page.get_pixmap(matrix=trans, alpha=False)
-        pm.save(imgPath + str(pg) + ".png")
+        pm.save(img_save_dir + str(pg) + ".png")
         print(f"page {pg}")
     pdf.close()
 
