e1a69d3f9c98ce8c2be7c8468eb9616943139c49
liwei <unix.lw@qq.com>
2023-11-28 22:46:22 +0800

    move and rename

--- Python/scripts/move_file.py
+++ Python/scripts/move_file.py
@@ -7,6 +7,8 @@ import random
 import string
 from functools import cmp_to_key
 
+VIDEO_RE = re.compile(r"(.mp4|.avi|.mkv|.rmvb)$", re.IGNORECASE)
+
 
 def search(filepath, file_list):
     try:
@@ -19,56 +21,85 @@ def search(filepath, file_list):
         if os.path.isdir(item_path):
             search(item_path, file_list)
         else:
-            print(item_path)
+            # print(item_path)
             file_list.append(item_path)
 
 
-def move():
+def move(src_dir, dest_dir, scan_pattern=VIDEO_RE):
+    """移动src_dir目录下的scan_pattern文件名到dest_dir.
+
+    Args:
+        src_dir (str): 源目录
+        dest_dir (str): 目标目录
+        scan_pattern (re, optional): 需要移动的文件名正则模式. Defaults to VIDEO_RE.
+    """    '''
+    '''
+
+    file_list = []
+    search(src_dir, file_list)
+
     for file_fullpath in file_list:
         file_name = os.path.basename(file_fullpath)
         if re.search(scan_pattern, file_name):
-            target_name = os.path.join(target_dir, file_name)
+            target_name = os.path.join(dest_dir, file_name)
             if os.path.exists(target_name):
-                random_name = "".join(random.sample(string.ascii_letters + string.digits, 8))
-                os.rename(target_name, os.path.join(target_dir, random_name))
-            shutil.move(file_fullpath, target_dir)
+                random_name = "".join(random.sample(
+                    string.ascii_letters + string.digits, 8)
+                )
+                os.rename(target_name, os.path.join(dest_dir, random_name))
+            shutil.move(file_fullpath, dest_dir)
 
 
-def compare(x, y):
-    stat_x = os.stat(scan_dir + "/" + x)
-    stat_y = os.stat(scan_dir + "/" + y)
-    if stat_x.st_mtime < stat_y.st_mtime:
-        return -1
-    elif stat_x.st_mtime > stat_y.st_mtime:
-        return 1
-    else:
-        return 0
+def _compare_wapper(scan_dir):
+
+    def compare(x, y):
+        stat_x = os.stat(scan_dir + "/" + x)
+        stat_y = os.stat(scan_dir + "/" + y)
+        if stat_x.st_mtime < stat_y.st_mtime:
+            return -1
+        elif stat_x.st_mtime > stat_y.st_mtime:
+            return 1
+        else:
+            return 0
+    return compare
 
 
-def rename():
+def rename(scan_dir, name_prefix='ABC-', random_name_len=8):
+    """文件重命名。name_prefix=None， 则采用随机名。
+    Args:
+        scan_dir (str): 文件所在目录
+        name_prefix (str, optional): 文件前缀. Defaults to 'ABC-'.
+        random_name_len (int, optional): 随机名长度. Defaults to 8.
+    """
+    compare = _compare_wapper(scan_dir)
+
     i = 1
-    name_prefix = 'ABC-'
-    reg_rename = re.compile(r'(.+)(.mp4|.avi|.mkv)$', re.IGNORECASE)
+
+    reg_rename = re.compile(r'(.+)(\.[^.]+)$', re.IGNORECASE)
     myfilelist = os.listdir(scan_dir)
     myfilelist = sorted(myfilelist, key=cmp_to_key(compare))
+
     for file_name in myfilelist:
 
         file_name_old_path = os.path.join(scan_dir, file_name)
-        new_name = re.sub(reg_rename, name_prefix +
-                          ("%03d" % i) + r'\2', file_name)
-        # new_name = "".join(random.sample(string.ascii_letters + string.digits, 8))
+
+        if name_prefix:
+            new_name = re.sub(reg_rename, name_prefix +
+                              ("%03d" % i) + r'\2', file_name)
+        else:
+            new_name = re.sub(reg_rename,
+                              "".join(random.sample(
+                                  string.ascii_letters + string.digits, random_name_len)
+                              ) + r'\2',
+                              file_name
+                              )
+
         file_name_new_path = os.path.join(scan_dir, new_name)
-        print(file_name_new_path)
+        # print(file_name_new_path)
         os.rename(file_name_old_path, file_name_new_path)
         i = i + 1
 
 
-scan_dir = r'G:\dir_to_scan'
-target_dir = r'G:\dir_to_save'
-
-scan_pattern = re.compile(r"(.mp4|.avi|.mkv)$", re.IGNORECASE)
-
-file_list = []
-
 if __name__ == "__main__":
-    pass
+    move(r"F:\src_dir", r"F:\dest_dir")
+    rename(r"F:\dest_dir", name_prefix=None)
