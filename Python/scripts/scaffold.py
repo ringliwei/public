8f76c540c4006b4b9d6b2c3b54524ae006b0ff89
liwei <unix.lw@qq.com>
2024-01-23 14:43:17 +0800

    python: scaffold.py

---
+++ Python/scripts/scaffold.py
@@ -0,0 +1,53 @@
+import os
+
+import argparse
+import logging
+import logging.handlers as h
+import pathlib
+
+
+SCRIPT_DIR = os.path.split(os.path.realpath(__file__))[0]
+
+LOG_DIR = pathlib.Path(f"{SCRIPT_DIR}/logs")
+
+if not LOG_DIR.exists():
+    LOG_DIR.mkdir(parents=True, exist_ok=True)
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s',
+                    level=logging.DEBUG,
+                    handlers=[logging.StreamHandler(), h.RotatingFileHandler(f'{LOG_DIR}/scaffold.log', encoding='utf8')])
+
+
+def command_line():
+    parser = argparse.ArgumentParser()
+
+    parser.description = 'This is a tool scaffold.'
+
+    parser.add_argument(
+        "-v", "--version-name",
+        help='VersionName',
+        required=False,
+        type=str
+    )
+
+    parser.add_argument(
+        "-p", "--with-production", help='''production env''', action="store_true")
+
+    parser.add_argument(
+        "-d", "--debug", help='''debug mode''', action="store_true")
+
+    args = parser.parse_args()
+    version_name = args.version_name if args.version_name else ""
+    debug = True if args.debug else False
+
+    logging.debug(f"debug: {debug}") if debug else ""
+    logging.debug(f"version_name: {version_name}") if version_name else ""
+
+    return args
+
+
+if __name__ == "__main__":
+
+    args = command_line()
+
+    logging.info("Hello, World!")
